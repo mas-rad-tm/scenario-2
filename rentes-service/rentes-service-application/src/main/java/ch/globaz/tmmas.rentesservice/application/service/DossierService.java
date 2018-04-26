1c0f64e13fd51cd3a7c055894e4df12ed8f192d9
Sébastien Chèvre <seb.chevre@gmail.com>
2018-04-26 20:10:52 +0200

    ok branch

--- rentes-service/rentes-service-application/src/main/java/ch/globaz/tmmas.rentesservice/application/service/DossierService.java
+++ rentes-service/rentes-service-application/src/main/java/ch/globaz/tmmas.rentesservice/application/service/DossierService.java
@@ -6,11 +6,13 @@ import ch.globaz.tmmas.rentesservice.domain.command.*;
 import ch.globaz.tmmas.rentesservice.domain.model.dossier.Dossier;
 import ch.globaz.tmmas.rentesservice.infrastructure.spi.PersonnesServiceResponseException;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public interface DossierService {
 
@@ -23,7 +25,7 @@ public interface DossierService {
 
 
 	@Transactional
-	Dossier creerDossierWithPersonne(CreerDossierWithPersonneCommand command) throws IOException, PersonnesServiceResponseException;
+	Dossier creerDossierWithPersonne(CreerDossierWithPersonneCommand command, CopyOnWriteArrayList<SseEmitter> consommateurs) throws IOException, PersonnesServiceResponseException;
 
 	Optional<DossierResourceAttributes> miseAJourDossier(MiseAJourDossierCommand command, Long dossierId);
 
